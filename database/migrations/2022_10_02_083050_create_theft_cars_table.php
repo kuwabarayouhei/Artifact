a9a28e5eb203f136e3681d0264fb7e2d3fca2340
kuwabarayouhei <kuwayou.syukatu@gmail.com>
2022-10-03 22:04:55 +0900

    環境構築

---
+++ database/migrations/2022_10_02_083050_create_theft_cars_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTheftCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('theft_cars', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 100);
+            $table->string('model', 50);
+            $table->string('color', 50);
+            $table->string('number', 50);
+            $table->string('location', 100);
+            $table->string('time', 50);
+            $table->string('situation', 200);
+            $table->text('information');
+            $table->string('image', 50)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('theft_cars');
+    }
+}
